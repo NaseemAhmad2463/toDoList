21c484f573e4d78924840161ee0a401339577a07
Naseem Ahmad <naseemgoogl786@gmail.com>
2022-12-05 23:27:57 +0530

    Version 1.1

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 const bodyParser = require("body-parser");
 const _ = require("lodash");
 
@@ -6,6 +7,8 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
